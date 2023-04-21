8465f27df4915704ae0a335bef43991e3c30841d
matanini <matanini@gmail.com>
2023-04-21 13:07:39 +0300

    update add_model page in front

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, HTTPException, UploadFile, File
-from typing import List
+from typing import List, Annotated, TextIO
+
+import shutil
 import os
 from PIL import Image
 import io
@@ -7,28 +9,45 @@ import io
 app = FastAPI()
 SHARED_DATA_PATH = "/usr/src/shared-volume/"
 
+app.add_event_handler("startup", lambda: os.makedirs(f"{SHARED_DATA_PATH}/images", exist_ok=True))
+app.add_event_handler("startup", lambda: os.makedirs(f"{SHARED_DATA_PATH}/model", exist_ok=True))
+
+
+
 @app.get("/")
 async def root():
     return {"message": "Backend is running"}
 
 
-@app.post("/model/add_class")
-async def add_class(label: str, data: List[UploadFile] = File(...)):
-    try:
-        os.mkdir(f"{SHARED_DATA_PATH}/images/{label}")
-    except:
-        pass
-    print(data)
-    for img in data:
-        img_file = Image.open(img)
-        img_file.save(f"{SHARED_DATA_PATH}/images/{label}/{img.filename}")
-    # send to model service
-    return {"message": "Class added successfully"}
+@app.post("/model/classes/add")
+async def add_class(label: str, number_of_images: int):
+
+    return {"message": f"Class {label} added {number_of_images} images successfully"}
+    # return {"message": "Yes"}
+
+@app.post("/model/classes/delete")
+async def add_class(label: str):
+    
+    classes = os.listdir(f"{SHARED_DATA_PATH}/images")
+    if label not in classes:
+        raise HTTPException(status_code=400, detail="Class not found")
+    else:
+        shutil.rmtree(f"{SHARED_DATA_PATH}/images/{label}", ignore_errors=True )
+
+    return {"message": f"Class {label} deleted successfully"}
+
 
 @app.get("/model/classes")
 async def get_classes():
     try:
-        classes = os.listdir(f"{SHARED_DATA_PATH}/images")
+        gold_labels = os.listdir(f"{SHARED_DATA_PATH}/images")
+        for label in gold_labels:
+            print(label)
+            print(len(os.listdir(f"{SHARED_DATA_PATH}/images/{label}")))
+        classes = {class_ : len(os.listdir(f"{SHARED_DATA_PATH}/images/{class_}")) for class_ in os.listdir(f"{SHARED_DATA_PATH}/images")}
+        # os.listdir(f"{SHARED_DATA_PATH}/images")
+        
+        # print(classes)
     except:
         raise HTTPException(status_code=400, detail="No classes found")
     return {"classes": classes }
--- frontend/main.py
+++ frontend/main.py
@@ -3,7 +3,7 @@ import httpx
 from fastapi import UploadFile
 from PIL import Image
 import os
-from io import StringIO
+from io import StringIO, BytesIO
 
 SHARED_DATA_PATH = "/usr/src/shared-volume/"
 
@@ -19,12 +19,50 @@ def test():
     st.write("Backend response: ", res.text)
     res = httpx.get("http://ml-service:8002/")
     st.write("ML-service response: ", res.text)
-    st.write("goni test")
-
+  
 
 def add_class():
-    st.title("Add Class")
-    st.write("Add a class to the model")
+    if 'add_class' in st.session_state and st.session_state['add_class']:
+        st.success("Class added successfully")
+        st.session_state['add_class'] = False
+    if 'delete_class' in st.session_state and st.session_state['delete_class']:
+        st.error("Class deleted successfully")
+        st.session_state['delete_class'] = False
+
+    st.title("Model Classes")
+    
+    num_of_class = httpx.get("http://backend:8001/model/classes")
+    num_of_class = num_of_class.json()['classes']
+    # st.write(num_of_class)
+    if len(num_of_class) == 0:
+        st.info("No classes found")
+    else:
+        # Tabs for each class
+        # sort the dict
+        keys = list(num_of_class.keys())
+        keys.sort()
+        num_of_class = {k: num_of_class[k] for k in keys}
+
+        # num_of_class = sorted(num_of_class)
+        tabs = st.tabs(num_of_class)
+
+        for i, (gold_label, img_count) in enumerate(num_of_class.items()):
+            tabs[i].subheader(f"Class: {gold_label}")
+            tabs[i].write(f"Number of images: {img_count}")
+
+            if tabs[i].checkbox("Delete Class", key=f"cb_delete_class_{i}"):
+                
+                delete = tabs[i].button("Delete Class", key=f"delete_class_{i}")
+                if delete:
+                    # delete the class
+                    res = httpx.post("http://backend:8001/model/classes/delete", params={"label": gold_label})
+                    # st.info(f"Class {gold_label} deleted")
+                    st.session_state['delete_class'] = True
+                    st.experimental_rerun()
+
+    st.markdown("""---""")
+    # 
+    st.write("Add a class to the model:")
     label = st.text_input("Label")
     data = st.file_uploader("Data", type=["jpg", "png"], accept_multiple_files=True)
     if st.button("Add Class"):
@@ -33,19 +71,18 @@ def add_class():
         elif len(data) == 0:
             st.error("Data cannot be empty")
         else:
-            files_dict = {}
-        for i, file in enumerate(data):
-            files_dict[f"file_{i}"] = (file.filename, file.file)
-            # data = [UploadFile(filename=img.name, file=img, size=img.size ) for img in data]
-            # for img in data:
+            # save the images in shared volume
+            os.makedirs(f"{SHARED_DATA_PATH}/images/{label}", exist_ok=True)
+            for img in data:
+                img_file = Image.open(img)
+                img_file.save(f"{SHARED_DATA_PATH}/images/{label}/{img.name}")
 
-            res = httpx.post("http://backend:8001/model/add_class", data={"label": label, "data": data})
-            st.success("Class added successfully")
+            res = httpx.post("http://backend:8001/model/classes/add", params={"label": label, 'number_of_images': len(data)})
+            # refresh the page
+            # st.experimental_rerun()
+            st.session_state['add_class'] = True
+            st.experimental_rerun()
 
-    # show all classes
-    with st.expander("See classes"):
-        res = httpx.get("http://backend:8001/model/classes")
-        st.write(res.text)
 
 
 def train():
